aa58e3f371c5c538f825dc3b1f27415fe00091f6
Parfenov Mikhail <mishaslsk@gmail.com>
2026-05-02 17:46:56 +0300

    Add upgrades Lable

--- Assets/Scripts/UI/UpgradeSlotUI.cs
+++ Assets/Scripts/UI/UpgradeSlotUI.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UpgradeSlotUI : MonoBehaviour
 {
     public Image[] slotIcons;
+    public TMP_Text[] slotLabels;
     public Button[] removeButtons;
+    public Color filledColor = new Color(0.2f, 0.8f, 0.4f, 1f);
+    public Color emptyColor = new Color(1, 1, 1, 0.2f);
 
     private UpgradeInventory inventory;
 
     void Start()
     {
         inventory = FindFirstObjectByType<UpgradeInventory>();
+        if (inventory == null)
+        {
+            Debug.LogWarning("UpgradeSlotUI: UpgradeInventory not found");
+            return;
+        }
         inventory.OnInventoryChanged += Refresh;
 
         for (int i = 0; i < removeButtons.Length; i++)
@@ -28,14 +37,40 @@ public class UpgradeSlotUI : MonoBehaviour
         {
             if (i < inventory.ActiveUpgrades.Count)
             {
-                slotIcons[i].sprite = inventory.ActiveUpgrades[i].icon;
-                slotIcons[i].color = Color.white;
+                var upgrade = inventory.ActiveUpgrades[i];
+
+                if (upgrade.icon != null)
+                {
+                    slotIcons[i].sprite = upgrade.icon;
+                    slotIcons[i].color = Color.white;
+                }
+                else
+                {
+                    slotIcons[i].sprite = null;
+                    slotIcons[i].color = filledColor;
+                }
+
+                if (i < slotLabels.Length && slotLabels[i] != null)
+                {
+                    slotLabels[i].text = !string.IsNullOrEmpty(upgrade.upgradeName)
+                        ? upgrade.upgradeName
+                        : upgrade.name;
+                    slotLabels[i].enabled = true;
+                }
+
                 removeButtons[i].gameObject.SetActive(true);
             }
             else
             {
                 slotIcons[i].sprite = null;
-                slotIcons[i].color = new Color(1, 1, 1, 0.2f);
+                slotIcons[i].color = emptyColor;
+
+                if (i < slotLabels.Length && slotLabels[i] != null)
+                {
+                    slotLabels[i].text = "";
+                    slotLabels[i].enabled = false;
+                }
+
                 removeButtons[i].gameObject.SetActive(false);
             }
         }
